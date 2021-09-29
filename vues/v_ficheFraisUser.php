57574624f26625868c6843d081e24c4e98c440a5
Paulhenrisolat <paulhenrisolat@gmail.com>
2021-09-29 17:33:02 +0200

    Validation frais v2
    v2

--- vues/v_ficheFraisUser.php
+++ vues/v_ficheFraisUser.php
@@ -1,9 +1,26 @@
 <?php
 /** Page de vue des admin pour les fiches de frais **/
+
 ?>
 
 
 <h2>Validation des fiches de frais</h2>
-<div class="row">
+<h3>Elément forfaitisés</h3>
+
+    <a>Forfait Etape</a>
+    <p><input type="number" maxlength="25"/></p>
+    <a>Frais Kilométrique</a>
+    <p><input type="number" maxlength="25"/></p>
+    <a>Nuité Hôtel</a>
+    <p><input type="number" maxlength="25"/></p>
+    <a>Repas Restaurant</a>
+    <p><input type="number" maxlength="25"/></p>
     
-</div>
+    <button class="btn btn-success" type="submit">Corriger</button>
+    <button class="btn btn-danger" type="reset">Réinitialiser</button>
+    <hr>
+    <p>Nombre de justificatifs : <input type="number"/></p>
+    
+    <button class="btn btn-success" type="submit">Valider</button>
+    <button class="btn btn-danger" type="reset">Réinitialiser</button>
+
