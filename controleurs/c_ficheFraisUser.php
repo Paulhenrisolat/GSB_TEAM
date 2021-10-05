4154cc90f2de68d13c5813f670c9be27951e4d5d
PGLucas <garambois.lucas@gmail.com>
2021-10-05 15:26:26 +0200

    Avancement mineur sur la récup de donnée d'une liste déroulante

--- controleurs/c_ficheFraisUser.php
+++ controleurs/c_ficheFraisUser.php
@@ -5,13 +5,15 @@
 include'vues/v_ficheFraisUser.php';
 ?>
 <label for="exampleDataList" class="form-label">Liste des utilisateurs</label>
-<input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Utilisateur">
-<datalist id="datalistOptions">
+<input class="form-control" list="ListUtilisateur" id="IdUtilisateur" placeholder="Utilisateur">
+<datalist id="ListUtilisateur", style="width:535px">
     <?php 
     foreach($pdo->GetUtilisateur() as &$value)
     {
         ?> <option value=<?php echo $value[0], '&nbsp;', $value[1] ; ?>> <?php 
     }
+    
     ?>
     
 </datalist>
+<input name="OK" type="button" value="Ok">
