66b29c82b3243143a7c0911882193855e782ad95
PGLucas <garambois.lucas@gmail.com>
2021-10-06 17:24:46 +0200

    Mise à jour du contrôler ficheFraisUser

--- controleurs/c_ficheFraisUser.php
+++ controleurs/c_ficheFraisUser.php
@@ -3,17 +3,29 @@
 //fiches frais des users
 
 include'vues/v_ficheFraisUser.php';
-?>
-<label for="exampleDataList" class="form-label">Liste des utilisateurs</label>
-<input class="form-control" list="ListUtilisateur" id="IdUtilisateur" placeholder="Utilisateur">
-<datalist id="ListUtilisateur", style="width:535px">
-    <?php 
-    foreach($pdo->GetUtilisateur() as &$value)
+
+    $value = $pdo->GetUtilisateur();
+    foreach()
     {
-        ?> <option value=<?php echo $value[0], '&nbsp;', $value[1] ; ?>> <?php 
+        ?> <option value= <?php $i ?> > <?php echo $value[0], '&nbsp;', $value[1] ;
+        $i++;
     }
     
     ?>
-    
-</datalist>
-<input name="OK" type="button" value="Ok">
+      <input type="submit" value="Envoyer" />
+           </p>
+</form>
+
+       <select name="IdMois">
+           <?php 
+          $i1=0;
+          $mois = filter_input(INPUT_POST, 'IdUtilisateur');
+    foreach($pdo->getLesMoisDisponibles($mois) as &$value1)
+    {
+        ?> <option value=<?php$i1?><?php echo $value1;
+        $i++;
+    }
+ 
+
+                
+
