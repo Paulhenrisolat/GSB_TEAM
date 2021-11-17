5d5572f60b84253c28c2b98e034fc2e7ccddfe38
Nicolas PREAUX <nicolas.preaux83@gmail.com>
2021-11-17 17:32:32 +0100

    Légère modification du .bat de génération de la doc php

--- tests/phpdoc/generate-phpDoc.bat
+++ tests/phpdoc/generate-phpDoc.bat
@@ -1,2 +1,2 @@
 @rd /s /q "documentation"
-php phpDocumentor.phar -d "C:\wamp64\www\gsb-ppe" -t "C:\wamp64\www\gsb-ppe\tests\phpdoc\documentation" docs/api
+php phpDocumentor.phar -d "C:\wamp64\www\gsb-ppe" -t "C:\wamp64\www\gsb-ppe\tests\phpdoc\documentation"
